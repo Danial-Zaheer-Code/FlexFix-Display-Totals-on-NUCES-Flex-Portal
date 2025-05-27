ed25e893fa9f3aef0c5824e41d8cac5ec9392f2d
Muhammad Maarij <mmaarij@ssidecisions.com>
2025-05-27 19:43:39 +0500

    Updated to Manifest v3

--- FlexFix/background.js
+++ FlexFix/background.js
@@ -1,35 +1,33 @@
-// This is the background code...
-
-// Listen for the browserAction to be clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-  // For the current tab, inject the "inject.js" file & execute it
-  if (tab.url.startsWith('https://flexstudent.nu.edu.pk/Student/StudentMarks'))
-  {
-	  chrome.tabs.executeScript(tab.id, {
-		file: 'inject.js'
-	  });
-	}
-});
-
-// listen for tab update events
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  // check if the page has finished loading
-  if (changeInfo.status === 'complete' && tab.url.startsWith('https://flexstudent.nu.edu.pk/Student/StudentMarks')) {
-    // inject the "inject.js" file & execute it
-    chrome.tabs.executeScript(tabId, {
-      file: 'inject.js'
+// When the extension icon is clicked
+chrome.action.onClicked.addListener((tab) => {
+  if (tab.url.startsWith('https://flexstudent.nu.edu.pk/Student/StudentMarks')) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['inject.js']
     });
   }
 });
 
+// When a tab is updated
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status === 'complete' &&
+    tab.url &&
+    tab.url.startsWith('https://flexstudent.nu.edu.pk/Student/StudentMarks')
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ['inject.js']
+    });
+  }
+});
 
-// Listen for content script messages
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  // Check if the message indicates a page change
-  if (message === 'pageChange') {
-    // Inject the "inject.js" file & execute it
-    chrome.tabs.executeScript(sender.tab.id, {
-      file: 'inject.js'
+// When a message is received from content script
+chrome.runtime.onMessage.addListener((message, sender) => {
+  if (message === 'pageChange' && sender.tab?.id) {
+    chrome.scripting.executeScript({
+      target: { tabId: sender.tab.id },
+      files: ['inject.js']
     });
   }
 });
