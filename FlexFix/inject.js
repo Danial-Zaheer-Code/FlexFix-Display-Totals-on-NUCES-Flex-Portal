61b7fbf3ebdddfe287c5547333430d9e9ac9031e
Danial-Zaheer-Code <danialzaheer80@gmail.com>
2026-01-05 18:17:59 +0500

    Renamed weightRow to rowWeight.

--- FlexFix/inject.js
+++ FlexFix/inject.js
@@ -52,7 +52,6 @@
   }
 
 
-
   // Find the div with class "active"
   const activeDiv = document.querySelector('.tab-pane.active');
 
@@ -60,31 +59,37 @@
   let totalWeightage = 0;
   let totalObtMarks = 0;
   let totalAverage = 0
+
   // get all marks table
   let tables = activeDiv.querySelectorAll('.sum_table');
+
   // iterate over all tables
   for (var i = 0; i < tables.length; i++) {
     var table = tables[i];
+
     // get all rows in the table
     var rows = table.querySelectorAll(".calculationrow");
     let rowCalculatedAverage = 0
     let tableWeightageSum = 0
+
     // iterate over all rows
     for (var j = 0; j < rows.length; j++) {
+
       // extract all weightage, average and total marks elements
-      var weightRow = rows[j].querySelectorAll(".weightage");
+      var rowWeight = rows[j].querySelectorAll(".weightage");
       var averageRow = rows[j].querySelectorAll(".AverageMarks");
       var totalMarksRow = rows[j].querySelectorAll(".GrandTotal");
-      if (weightRow.length == 0 || weightRow[0].textContent == "0 " ||
+      if (rowWeight.length == 0 || rowWeight[0].textContent == "0 " ||
         averageRow.length == 0 || averageRow[0].textContent == "0 " ||
         totalMarksRow.length == 0 || totalMarksRow[0].textContent == "0 ") {
         continue
       } else {
         // calculate the average for the row
-        tableWeightageSum += parseFloat(weightRow[0].textContent);
-        rowCalculatedAverage += (parseFloat(averageRow[0].textContent) / parseFloat(totalMarksRow[0].textContent)) * parseFloat(weightRow[0].textContent);
+        tableWeightageSum += parseFloat(rowWeight[0].textContent);
+        rowCalculatedAverage += (parseFloat(averageRow[0].textContent) / parseFloat(totalMarksRow[0].textContent)) * parseFloat(rowWeight[0].textContent);
       }
     }
+
     // calculate the average for the table
     var totalSection = table.querySelectorAll('[class*="totalColumn_"]');
     if (totalSection.length == 1 && tableWeightageSum != 0 && rowCalculatedAverage != 0 && totalSection[0].querySelectorAll(".totalColweightage").length == 1) {
@@ -92,6 +97,7 @@
       rowCalculatedAverage = rowCalculatedAverage / tableWeightageSum * parseFloat(tableColWeigtage[0].textContent);
       totalAverage += rowCalculatedAverage;
     }
+
     // calculate the total and obtained marks for the table
     if (totalSection.length == 1) {
       var _tableColWeigtage = totalSection[0].querySelectorAll(".totalColweightage")
