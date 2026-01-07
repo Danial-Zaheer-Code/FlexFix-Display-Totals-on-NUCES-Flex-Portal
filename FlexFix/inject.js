1fd63d203507e44d9f57f18f66dee20e2117d29b
Muhammad Danial Zaheer <danialzaheer80@gmail.com>
2026-01-07 05:46:05 +0500

    Code Refactoring to Improve Readability and Prepare for New Features (#3)
    * Added a method to check for the existance of an element.
    
    * Created a helper method to create the an element.
    
    * Renamed weightRow to rowWeight.
    
    * Corrected Formatting.
    
    * Renamed avergeRow to rowAverageMarks.
    
    * Renamed totalMarks row to rowtotalMarks.
    
    * Changed rowWeight, rowAverageMarks and rowTtalMarks from var to const..
    
    * Extracted the check for rowWeight, rowAverageMarks, and rowTotalMarks to a helper method.
    
    * Moved code outsicde of  unnecessary else block to reduce nesting.
    
    * Changed table and rows local variable to const.
    
    * Changed all immutable local variables from var to const.
    
    * Removed unnecessary commnents.
    
    * Added early continue refactoring to remove duplicated checks.
    
    * Removed dublicated a dublicated variable and used early continue refactoring on some condition.
    
    * Renamed all variable ending with MarksElement to AbsolutesElement.
    
    * Removed some extra spaces.
    
    * Renamed totalObtMarks to totalObtainedAbsolutes and totalAverage to totalAverageAbsolutes..
    
    * Changed loop counters from var to let and chaged final calculated average from var to const.
    
    * Added 'Absolutes' at the last of variable names that were representing absolutes count.
    
    * Added high level comments to improve readability.
    
    * Applied extract variables on current row average absolutes caclulation.
    
    * Renamed rowsCalculatedAverageAbsolutes to rowsTotalAverageAbsolutes.

--- FlexFix/inject.js
+++ FlexFix/inject.js
@@ -1,122 +1,140 @@
 (function () {
-  // Find the div with class "m-portlet"
-  const portlet = document.querySelector('.m-portlet');
 
-  // Check if the total and obtained marks elements already exist
-  let totalMarksElement = portlet.querySelector('h5[data-type="total"]');
-  let obtainedMarksElement = portlet.querySelector('h5[data-type="obtained"]');
-  let classAverageElement = portlet.querySelector('h5[data-type="classAverage"]');
-  let redirectButton = portlet.querySelector('button[data-type="redirect"]');
+	//Get the div to display the output.
+	const portlet = document.querySelector('.m-portlet');
 
-  // If the elements don't exist, create new ones and append them to the portlet
-  if (!totalMarksElement) {
-    totalMarksElement = document.createElement('h5');
-    totalMarksElement.setAttribute('data-type', 'total');
-    totalMarksElement.style.color = 'white';
-    totalMarksElement.style.marginLeft = '30px';
-    totalMarksElement.style.marginTop = '7px';
-    portlet.appendChild(totalMarksElement);
-  }
+	//Get all the elements of output div.
+	let totalAbsolutesElement = portlet.querySelector('h5[data-type="total"]');
+	let obtainedAbsolutesElement = portlet.querySelector('h5[data-type="obtained"]');
+	let averageAbsolutesElement = portlet.querySelector('h5[data-type="classAverage"]');
+	let redirectButton = portlet.querySelector('button[data-type="redirect"]');
 
-  if (!obtainedMarksElement) {
-    obtainedMarksElement = document.createElement('h5');
-    obtainedMarksElement.setAttribute('data-type', 'obtained');
-    obtainedMarksElement.style.color = 'white';
-    obtainedMarksElement.style.marginLeft = '30px';
-    portlet.appendChild(obtainedMarksElement);
-  }
+	//If any element don't exist then add it into output div.
+	if (!isElementExist(totalAbsolutesElement)) {
+		totalAbsolutesElement = createElement("h5", "data-type", "total");
+		totalAbsolutesElement.style.marginTop = '7px';
+		portlet.appendChild(totalAbsolutesElement);
+	}
 
-  if (!classAverageElement) {
-    classAverageElement = document.createElement('h5');
-    classAverageElement.setAttribute('data-type', 'classAverage');
-    classAverageElement.style.color = 'white';
-    classAverageElement.style.marginLeft = '30px';
-    portlet.appendChild(classAverageElement);
-  }
-  if (!redirectButton) {
-    redirectButton = document.createElement('button');
-    redirectButton.setAttribute('data-type', 'redirect');
-    redirectButton.style.color = 'black';
-    redirectButton.style.backgroundColor = 'white';
-    redirectButton.style.border = 'none';
-    redirectButton.style.borderRadius = '5px';
-    redirectButton.style.padding = '10px 20px';
-    redirectButton.style.fontSize = '16px';
-    redirectButton.style.fontWeight = 'bold';
-    redirectButton.textContent = 'Redirect to Grade Calculator';
+	if (!isElementExist(obtainedAbsolutesElement)) {
+		obtainedAbsolutesElement = createElement("h5", "data-type", "obtained");
+		portlet.appendChild(obtainedAbsolutesElement);
+	}
 
-    var containerDiv = document.createElement('div');
-    containerDiv.style.display = 'flex';
-    containerDiv.style.justifyContent = 'center';
-    containerDiv.style.alignItems = 'center';
-    containerDiv.style.height = '100%';
-    containerDiv.appendChild(redirectButton);
+	if (!isElementExist(averageAbsolutesElement)) {
+		averageAbsolutesElement = createElement("h5", "data-type", "classAverage");
+		portlet.appendChild(averageAbsolutesElement);
+	}
 
-    redirectButton.addEventListener('click', function () {
-      window.open('https://fastnugrader.netlify.app/', '_blank');
-    });
+	if (!isElementExist(redirectButton)) {
+		redirectButton = document.createElement('button');
+		redirectButton.setAttribute('data-type', 'redirect');
+		redirectButton.style.color = 'black';
+		redirectButton.style.backgroundColor = 'white';
+		redirectButton.style.border = 'none';
+		redirectButton.style.borderRadius = '5px';
+		redirectButton.style.padding = '10px 20px';
+		redirectButton.style.fontSize = '16px';
+		redirectButton.style.fontWeight = 'bold';
+		redirectButton.textContent = 'Redirect to Grade Calculator';
 
-    portlet.appendChild(containerDiv);
-  }
+		var containerDiv = document.createElement('div');
+		containerDiv.style.display = 'flex';
+		containerDiv.style.justifyContent = 'center';
+		containerDiv.style.alignItems = 'center';
+		containerDiv.style.height = '100%';
+		containerDiv.appendChild(redirectButton);
+
+		redirectButton.addEventListener('click', function () {
+			window.open('https://fastnugrader.netlify.app/', '_blank');
+		});
+
+		portlet.appendChild(containerDiv);
+	}
+
+	//Get the current opened subject.
+	const currentSubjectDiv = document.querySelector('.tab-pane.active');
+
+	let totalWeightage = 0;
+	let totalObtainedAbsolutes = 0;
+	let totalAverageAbsolutes = 0;
+
+	//Get all the evaluation tables for current subject. i.e Assignment, Quiz.
+	let tables = currentSubjectDiv.querySelectorAll('.sum_table');
+
+	//Iterate over all table for current subject.
+	for (let i = 0; i < tables.length; i++) {
+		const table = tables[i];
+
+		//Get all rows for this table.
+		const tableRows = table.querySelectorAll(".calculationrow");
+		let rowsTotalAverageAbsolutes = 0
+		let tableWeightageSum = 0
+
+		//Iterate over all the rows for this table
+		for (let j = 0; j < tableRows.length; j++) {
+
+			const rowWeight = tableRows[j].querySelectorAll(".weightage");
+			const rowAverageMarks = tableRows[j].querySelectorAll(".AverageMarks");
+			const rowTotalMarks = tableRows[j].querySelectorAll(".GrandTotal");
+
+			if (isEmptyOrZero(rowWeight) || isEmptyOrZero(rowAverageMarks) || isEmptyOrZero(rowTotalMarks)) {
+				continue;
+			}
+
+			tableWeightageSum += parseFloat(rowWeight[0].textContent);
+			const currentRowAverageAbsolutes = (parseFloat(rowAverageMarks[0].textContent) / parseFloat(rowTotalMarks[0].textContent)) * parseFloat(rowWeight[0].textContent);
+			rowsTotalAverageAbsolutes +=  currentRowAverageAbsolutes;
+		}
+
+		//Get the last row of each table. Which is the "total" row.
+		const totalSection = table.querySelectorAll('[class*="totalColumn_"]');
+
+		//Only one element should be returned
+		if (totalSection.length !== 1) {
+			continue;
+		}
 
 
+		const tableTotalWeigtage = totalSection[0].querySelectorAll(".totalColweightage");
+		const tableTotalObtainedAbsolutes = totalSection[0].querySelectorAll(".totalColObtMarks");
 
-  // Find the div with class "active"
-  const activeDiv = document.querySelector('.tab-pane.active');
+		//Only one element should be returned from both variables
+		if (tableTotalObtainedAbsolutes.length !== 1 || tableTotalWeigtage.length !== 1) {
+			continue;
+		}
 
-  // Initialize the total, obtained marks and average to 0
-  let totalWeightage = 0;
-  let totalObtMarks = 0;
-  let totalAverage = 0
-  // get all marks table
-  let tables = activeDiv.querySelectorAll('.sum_table');
-  // iterate over all tables
-  for (var i = 0; i < tables.length; i++) {
-    var table = tables[i];
-    // get all rows in the table
-    var rows = table.querySelectorAll(".calculationrow");
-    let rowCalculatedAverage = 0
-    let tableWeightageSum = 0
-    // iterate over all rows
-    for (var j = 0; j < rows.length; j++) {
-      // extract all weightage, average and total marks elements
-      var weightRow = rows[j].querySelectorAll(".weightage");
-      var averageRow = rows[j].querySelectorAll(".AverageMarks");
-      var totalMarksRow = rows[j].querySelectorAll(".GrandTotal");
-      if (weightRow.length == 0 || weightRow[0].textContent == "0 " ||
-        averageRow.length == 0 || averageRow[0].textContent == "0 " ||
-        totalMarksRow.length == 0 || totalMarksRow[0].textContent == "0 ") {
-        continue
-      } else {
-        // calculate the average for the row
-        tableWeightageSum += parseFloat(weightRow[0].textContent);
-        rowCalculatedAverage += (parseFloat(averageRow[0].textContent) / parseFloat(totalMarksRow[0].textContent)) * parseFloat(weightRow[0].textContent);
-      }
-    }
-    // calculate the average for the table
-    var totalSection = table.querySelectorAll('[class*="totalColumn_"]');
-    if (totalSection.length == 1 && tableWeightageSum != 0 && rowCalculatedAverage != 0 && totalSection[0].querySelectorAll(".totalColweightage").length == 1) {
-      var tableColWeigtage = totalSection[0].querySelectorAll(".totalColweightage")
-      rowCalculatedAverage = rowCalculatedAverage / tableWeightageSum * parseFloat(tableColWeigtage[0].textContent);
-      totalAverage += rowCalculatedAverage;
-    }
-    // calculate the total and obtained marks for the table
-    if (totalSection.length == 1) {
-      var _tableColWeigtage = totalSection[0].querySelectorAll(".totalColweightage")
-      var _tableColObtMarks = totalSection[0].querySelectorAll(".totalColObtMarks")
-      if (_tableColWeigtage.length == 1 && _tableColObtMarks.length == 1) {
-        totalWeightage += parseFloat(_tableColWeigtage[0].textContent)
-        totalObtMarks += parseFloat(_tableColObtMarks[0].textContent)
-      }
-    }
-  }
-  // check if average was calculated or not
-  var finalCalculateAverage = isNaN(totalAverage) ? "Cannot Calculate, Missing Data" : totalAverage.toFixed(2)
+		//Handle best off average.
+		if (tableWeightageSum != 0) {
+			rowsTotalAverageAbsolutes = rowsTotalAverageAbsolutes / tableWeightageSum * parseFloat(tableTotalWeigtage[0].textContent);
+			totalAverageAbsolutes += rowsTotalAverageAbsolutes;
+		}
 
-  // Update the content of the total and obtained marks elements
-  totalMarksElement.textContent = 'Total Absolutes: ' + totalWeightage.toFixed(2);
-  obtainedMarksElement.textContent = 'Obtained Absolutes: ' + totalObtMarks.toFixed(2);
-  classAverageElement.textContent = 'Class Average: ' + finalCalculateAverage;
+		totalWeightage += parseFloat(tableTotalWeigtage[0].textContent)
+		totalObtainedAbsolutes += parseFloat(tableTotalObtainedAbsolutes[0].textContent)
+	}
 
-  chrome.runtime.sendMessage('pageChange');
+	const finalTotalAverageAbsolutes = isNaN(totalAverageAbsolutes) ? "Cannot Calculate, Missing Data" : totalAverageAbsolutes.toFixed(2)
+
+	totalAbsolutesElement.textContent = 'Total Absolutes: ' + totalWeightage.toFixed(2);
+	obtainedAbsolutesElement.textContent = 'Obtained Absolutes: ' + totalObtainedAbsolutes.toFixed(2);
+	averageAbsolutesElement.textContent = 'Class Average: ' + finalTotalAverageAbsolutes;
+
+	chrome.runtime.sendMessage('pageChange');
 })();
+
+function isElementExist(element) {
+	return element !== null && element !== undefined;
+}
+
+function createElement(tag, attribute, value) {
+	const element = document.createElement(tag);
+	element.setAttribute(attribute, value);
+	element.style.color = 'white';
+	element.style.marginLeft = '30px';
+	return element;
+}
+
+function isEmptyOrZero(row) {
+	return row.length === 0 || row[0].textContent.trim() === "0";
+}
